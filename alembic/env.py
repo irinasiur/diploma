089ab79452ccde51d84d1c3c4013d099a62525bd
irinasiur <drozdovairina10@gmail.com>
2024-06-19 02:18:28 +0300

    исправления по замечаниям

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,7 @@
 import sys
 import os
+# Добавьте путь проекта в sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -8,15 +10,13 @@ from tasktracker.database import Base
 
 # from tasktracker.models import *  # Employee, Task
 
-# Добавьте путь проекта в sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 # from tasktracker.models import Employee, Task  # Импортируйте необходимые модели
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
